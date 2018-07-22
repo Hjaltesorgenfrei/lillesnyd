f0a8f30a92199f544f3c5b191fa3b734adcec5ff
Hjalte Mac Dalland <hjaltesorgenfrei@gmail.com>
2018-07-22 18:51:40 +0200

    Testing json

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
-
+#include <fstream>
+#include <string>
 
 // for convenience
 using json = nlohmann::json;
@@ -12,7 +13,24 @@ int main() {
 
 	std::cout << j["pi"] << std::endl;
 
+
 	std::cin.get();
+	// add a string that is stored as std::string
+	j["name"] = "Niels";
 
+	// add another null object by passing nullptr
+	j["nothing"] = nullptr;
 
+	// add an object inside the object
+	j["answer"]["everything"] = 42;
+
+	// add an array that is stored as std::vector (using an initializer list)
+	j["list"] = { 1, 0, 2 };
+
+	// add another object (using an initializer list of pairs)
+	j["object"] = { { "currency", "USD" },{ "value", 42.99 } };
+
+	// write prettified JSON to another file
+	std::ofstream o("pretty.json");
+	o << std::setw(4) << j << std::endl;
 }
