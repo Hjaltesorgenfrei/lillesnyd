2fada2fb81e45c06c856a8fd8a710bbd6795f7ef
Hjalte Mac Dalland <hjaltesorgenfrei@gmail.com>
2018-07-26 18:38:14 +0200

    Offset from json seperated into function and variables renamed for readability.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,92 @@
 #include "processtools.h"
 
 
+int GetOffsetFromJson(json& j, std::string name, const wchar_t * exeName, const wchar_t* moduleName) {
+	try {
+		json PatternElement = jsonArrayValueFinder(j, "signatures", "name", name);
+
+		std::string patternString = PatternElement["pattern"];
+		//TODO: Change this so that it does not create this hex vector first.
+		std::vector<std::pair<char, bool>> PatternVector = convertToHexVector(patternString.c_str());
+		//Get Process ID by enumerating the processes using tlhelp32snapshot
+		DWORD processID = GetProcID(exeName);
+		//Get handle by OpenProcess
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
+		{
+			MODULEENTRY32 modEntry = GetModule(processID, moduleName);
+			//PatternScan
+			char * patternChar = VectorToPattern(PatternVector);
+			char * mask = VectorToMask(PatternVector);
+			void * pointerAddress = PatternScanExModule(hProcess, exeName, moduleName, patternChar, mask);
+			LPVOID pBuffer;
+			const uintptr_t& point = (int)pointerAddress + PatternElement["offsets"][0]; //Gets an off by one error i think. 
+			ReadProcessMemory(hProcess, LPCVOID(point), &pBuffer, sizeof(uintptr_t), nullptr);
+			
+			#ifdef _DEBUG
+			std::cout << "This offset is: " << name << "\n";
+			std::cout << "Pattern as read from json: " << PatternElement["pattern"] << "\n";
+			std::cout << std::hex << "Pointer Address: " << pointerAddress << "\n";
+			std::cout << std::hex << "Module Entry Address: " << (int)modEntry.modBaseAddr << "\n";
+			std::cout << std::hex << "Offset Address: " << (int)pBuffer - (int)modEntry.modBaseAddr << std::endl;
+			#endif
+			
+			return (int)pBuffer - (int)modEntry.modBaseAddr;
+		}
+	}
+	catch (std::runtime_error& runErr) {
+		std::cerr << runErr.what() << std::endl;
+	}
+	return NULL;
+}
+
+
+
+int GetOffsetFromJson(json& j, std::string name) {
+	try {
+
+		json PatternElement = jsonArrayValueFinder(j, "signatures", "name", name);
+
+		std::string str = j["executable"];
+		std::wstring widestr = std::wstring(str.begin(), str.end());
+		const wchar_t * exeName = widestr.c_str();
+
+		std::string str2 = PatternElement["module"];
+		std::wstring widestr2 = std::wstring(str2.begin(), str2.end());
+		const wchar_t * moduleName = widestr2.c_str();
+		
+		std::string patternString = PatternElement["pattern"];
+		//TODO: Change this so that it does not create this hex vector first.
+		std::vector<std::pair<char, bool>> PatternVector = convertToHexVector(patternString.c_str());
+		//Get Process ID by enumerating the processes using tlhelp32snapshot
+		DWORD processID = GetProcID(exeName);
+		//Get handle by OpenProcess
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
+		{
+			MODULEENTRY32 modEntry = GetModule(processID, moduleName);
+			//PatternScan
+			char * patternChar = VectorToPattern(PatternVector);
+			char * mask = VectorToMask(PatternVector);
+			void * pointerAddress = PatternScanExModule(hProcess, exeName, moduleName, patternChar, mask);
+			LPVOID pBuffer;
+			const uintptr_t& point = (int)pointerAddress + PatternElement["offsets"][0]; //Gets an off by one error i think. 
+			ReadProcessMemory(hProcess, LPCVOID(point), &pBuffer, sizeof(uintptr_t), nullptr);
+
+			#ifdef _DEBUG
+			std::cout << "This offset is: " << name << "\n";
+			std::cout << "Pattern as read from json: " << PatternElement["pattern"] << "\n";
+			std::cout << std::hex << "Pointer Address: " << pointerAddress << "\n";
+			std::cout << std::hex << "Module Entry Address: " << (int)modEntry.modBaseAddr << "\n";
+			std::cout << std::hex << "Offset Address: " << (int)pBuffer - (int)modEntry.modBaseAddr << std::endl;
+			#endif
+			
+			return (int)pBuffer - (int)modEntry.modBaseAddr;
+		}
+	}
+	catch (std::runtime_error& runErr) {
+		std::cerr << runErr.what() << std::endl;
+	}
+	return NULL;
+}
 
 int main()
 {
@@ -19,36 +105,13 @@ int main()
 	std::ifstream ifs("config.json");
 	json j = json::parse(ifs);
 
-	try {
-		json localplayer = jsonArrayValueFinder(j, "signatures", "name", "dwLocalPlayer");
-		std::cout << localplayer["pattern"] << std::endl;
-		std::vector<std::string> vec{ StringToVector(localplayer["pattern"], ' ') };
-		std::string patternString = localplayer["pattern"];
-		//print the vector
-		std::vector<std::pair<char, bool>> realnibba = convertToHexVector(patternString.c_str());
-		//Get Process ID by enumerating the processes using tlhelp32snapshot
-		DWORD processID = GetProcID(L"csgo.exe");
-		//Get handle by OpenProcess
-		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
-		{
-			MODULEENTRY32 modEntry = GetModule(processID, L"client.dll");
-			std::cout << "THIS IS THE LOCALPLAYER" << std::endl;
-			//PatternScan
-			char * patternChar = VectorToPattern(realnibba);
-			char * mask = VectorToMask(realnibba);
-			void * healthDecAddress = PatternScanExModule(hProcess, L"csgo.exe", L"client.dll", patternChar, mask);
-			std::cout << std::hex << healthDecAddress << std::endl;
-			LPVOID pBuffer;
-			const uintptr_t& point = (int)healthDecAddress + 1; //Gets an off by one error i think. 
-			ReadProcessMemory(hProcess, LPCVOID(point), &pBuffer, sizeof(uintptr_t), nullptr);
-			std::cout << std::hex << (int)modEntry.modBaseAddr << std::endl;
-			std::cout << std::hex << (int)pBuffer - (int)modEntry.modBaseAddr << std::endl;
-		}
-	}
-	catch (std::runtime_error& runErr) {
-		std::cerr << runErr.what() << std::endl;
-	}
+	GetOffsetFromJson(j, "dwLocalPlayer", L"csgo.exe", L"client.dll");
+	GetOffsetFromJson(j, "dwEntityList");
 
+	for (auto element : j["signatures"]) {
+		GetOffsetFromJson(j, element["name"]);
+		system("pause");
+	}
 
 	std::cin.get();
 	return 0;
