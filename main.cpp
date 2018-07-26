df595922ea47bf8d068b38367be38a3b9f0ce701
Hjalte Mac Dalland <hjaltesorgenfrei@gmail.com>
2018-07-26 19:14:46 +0200

    Relative working now.

--- main.cpp
+++ main.cpp
@@ -11,46 +11,6 @@
 #include "processtools.h"
 
 
-int GetOffsetFromJson(json& j, std::string name, const wchar_t * exeName, const wchar_t* moduleName) {
-	try {
-		json PatternElement = jsonArrayValueFinder(j, "signatures", "name", name);
-
-		std::string patternString = PatternElement["pattern"];
-		//TODO: Change this so that it does not create this hex vector first.
-		std::vector<std::pair<char, bool>> PatternVector = convertToHexVector(patternString.c_str());
-		//Get Process ID by enumerating the processes using tlhelp32snapshot
-		DWORD processID = GetProcID(exeName);
-		//Get handle by OpenProcess
-		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
-		{
-			MODULEENTRY32 modEntry = GetModule(processID, moduleName);
-			//PatternScan
-			char * patternChar = VectorToPattern(PatternVector);
-			char * mask = VectorToMask(PatternVector);
-			void * pointerAddress = PatternScanExModule(hProcess, exeName, moduleName, patternChar, mask);
-			LPVOID pBuffer;
-			const uintptr_t& point = (int)pointerAddress + PatternElement["offsets"][0]; //Gets an off by one error i think. 
-			ReadProcessMemory(hProcess, LPCVOID(point), &pBuffer, sizeof(uintptr_t), nullptr);
-			
-			#ifdef _DEBUG
-			std::cout << "This offset is: " << name << "\n";
-			std::cout << "Pattern as read from json: " << PatternElement["pattern"] << "\n";
-			std::cout << std::hex << "Pointer Address: " << pointerAddress << "\n";
-			std::cout << std::hex << "Module Entry Address: " << (int)modEntry.modBaseAddr << "\n";
-			std::cout << std::hex << "Offset Address: " << (int)pBuffer - (int)modEntry.modBaseAddr << std::endl;
-			#endif
-			
-			return (int)pBuffer - (int)modEntry.modBaseAddr;
-		}
-	}
-	catch (std::runtime_error& runErr) {
-		std::cerr << runErr.what() << std::endl;
-	}
-	return NULL;
-}
-
-
-
 int GetOffsetFromJson(json& j, std::string name) {
 	try {
 
@@ -78,18 +38,27 @@ int GetOffsetFromJson(json& j, std::string name) {
 			char * mask = VectorToMask(PatternVector);
 			void * pointerAddress = PatternScanExModule(hProcess, exeName, moduleName, patternChar, mask);
 			LPVOID pBuffer;
-			const uintptr_t& point = (int)pointerAddress + PatternElement["offsets"][0]; //Gets an off by one error i think. 
-			ReadProcessMemory(hProcess, LPCVOID(point), &pBuffer, sizeof(uintptr_t), nullptr);
 
+			if (PatternElement["offsets"].size()) { // if there is any offsets, add them
+				int totalOffset = 0;
+				for (int i = 0; i != PatternElement["offsets"].size(); i++)
+					totalOffset += PatternElement["offsets"][i];
+				ReadProcessMemory(hProcess, LPCVOID((int)pointerAddress + totalOffset), &pBuffer, sizeof(uintptr_t), nullptr);
+			}
+			else {
+				ReadProcessMemory(hProcess, LPCVOID((int)pointerAddress), &pBuffer, sizeof(uintptr_t), nullptr);
+			}
 			#ifdef _DEBUG
 			std::cout << "This offset is: " << name << "\n";
 			std::cout << "Pattern as read from json: " << PatternElement["pattern"] << "\n";
 			std::cout << std::hex << "Pointer Address: " << pointerAddress << "\n";
+			std::cout << std::hex << "Value read from Pointer Address: " << (int)pBuffer << "\n";
 			std::cout << std::hex << "Module Entry Address: " << (int)modEntry.modBaseAddr << "\n";
-			std::cout << std::hex << "Offset Address: " << (int)pBuffer - (int)modEntry.modBaseAddr << std::endl;
+			std::cout << std::hex << "Offset Address from module entry.: " << ((int)pBuffer - (int)modEntry.modBaseAddr) << std::endl;
 			#endif
-			
-			return (int)pBuffer - (int)modEntry.modBaseAddr;
+			if (element["relative"])
+				return (int)pBuffer - (int)modEntry.modBaseAddr;
+			else return (int)pBuffer
 		}
 	}
 	catch (std::runtime_error& runErr) {
@@ -105,12 +74,16 @@ int main()
 	std::ifstream ifs("config.json");
 	json j = json::parse(ifs);
 
-	GetOffsetFromJson(j, "dwLocalPlayer", L"csgo.exe", L"client.dll");
-	GetOffsetFromJson(j, "dwEntityList");
+	GetOffsetFromJson(j, "dwLocalPlayer");
 
 	for (auto element : j["signatures"]) {
-		GetOffsetFromJson(j, element["name"]);
-		system("pause");
+		if (element["relative"])
+			GetOffsetFromJson(j, element["name"]);
+	}
+	std::cout << "______________" << std::endl;
+	for (auto element : j["signatures"]) {
+		if (!element["relative"])
+			GetOffsetFromJson(j, element["name"]);
 	}
 
 	std::cin.get();
